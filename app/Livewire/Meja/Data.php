1ce256868500049d90c3fa743dc7dfff97cfb26e
Raihan Arpe <raihanarpe@gmail.com>
2024-02-26 10:47:40 +0700

    Feat: Add all livewire crud component

---
+++ app/Livewire/Meja/Data.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Meja;
+
+use Livewire\Component;
+
+class Data extends Component
+{
+    public function render()
+    {
+        return view('livewire.meja.data');
+    }
+}
